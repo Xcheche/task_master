dab48d68250c5dec52fea5c1cc4d08d365a07654
xcheche <chekwubechukwu23@gmail.com>
2024-06-21 17:32:31 +0100

    done

--- task/models.py
+++ task/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    is_done = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    # used for arranging the classes in orderly way
+    class Meta:
+        ordering = ("-created_at",)
--- task/views.py
+++ task/views.py
@@ -1,6 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+
+from .models import Task
+from .forms import TaskForm
 
 
 # Create your views here.
 def index(request):
-    return render(request, "task/index.html")
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+    form = TaskForm()
+    tasks = Task.objects.filter(is_done=False)
+    context = {"tasks": tasks, "form": form}
+
+    return render(request, "task/index.html", context=context)
+
+
+# deleting task view
+
+
+def delete_task(request, task_id):
+    task = Task.objects.get(pk=task_id)
+    task.delete()
+    return redirect("/")
+
+
+def mark_as_done(request, task_id):
+    tasks = Task.objects.get(pk=task_id)
+    tasks.is_done = True
+    tasks.save()
+    message = "Task marked as done"
+    return redirect("/")
